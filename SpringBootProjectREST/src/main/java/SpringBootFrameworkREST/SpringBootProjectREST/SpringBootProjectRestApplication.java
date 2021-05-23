1defe2ce3c69f11f428fda2f70e1e76a7b6c8396
vsaxena12 <vsaxena1@binghamton.edu>
2021-05-23 05:45:03 -0400

    Spring update with springboot'

---
+++ SpringBootProjectREST/src/main/java/SpringBootFrameworkREST/SpringBootProjectREST/SpringBootProjectRestApplication.java
@@ -0,0 +1,13 @@
+package SpringBootFrameworkREST.SpringBootProjectREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectRestApplication.class, args);
+	}
+
+}
--- SpringBootProjectREST/src/test/java/SpringBootFrameworkREST/SpringBootProjectREST/SpringBootProjectRestApplicationTests.java
+++ SpringBootProjectREST/src/test/java/SpringBootFrameworkREST/SpringBootProjectREST/SpringBootProjectRestApplicationTests.java
@@ -1,10 +1,10 @@
-package SpringBootProject.SpringBoot;
+package SpringBootFrameworkREST.SpringBootProjectREST;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class SpringBootProjectRestApplicationTests {
 
 	@Test
 	void contextLoads() {
