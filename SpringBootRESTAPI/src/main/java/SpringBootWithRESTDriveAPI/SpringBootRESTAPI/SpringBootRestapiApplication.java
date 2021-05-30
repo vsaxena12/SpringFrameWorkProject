3cf742663cdc0cce1ed1c576ac3366820f82e61f
vsaxena12 <vsaxena1@binghamton.edu>
2021-05-30 09:59:27 -0400

    update

---
+++ SpringBootRESTAPI/src/main/java/SpringBootWithRESTDriveAPI/SpringBootRESTAPI/SpringBootRestapiApplication.java
@@ -0,0 +1,13 @@
+package SpringBootWithRESTDriveAPI.SpringBootRESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestapiApplication.class, args);
+	}
+
+}
---
+++ SpringBootRESTAPI/src/test/java/SpringBootWithRESTDriveAPI/SpringBootRESTAPI/SpringBootRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringBootWithRESTDriveAPI.SpringBootRESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
