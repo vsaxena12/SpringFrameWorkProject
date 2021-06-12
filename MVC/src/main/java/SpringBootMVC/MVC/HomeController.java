c910711ef1c69ff64f570d94cee9d08c0c256e6d
vsaxena12 <vsaxena1@binghamton.edu>
2021-06-12 06:45:58 -0400

    update

--- MVC/src/main/java/SpringBootMVC/MVC/HomeController.java
+++ MVC/src/main/java/SpringBootMVC/MVC/HomeController.java
@@ -5,13 +5,12 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController 
 {
-	@RequestMapping("/")
+	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String home()
 	{
 		//System.out.println("Home Works");
@@ -20,20 +19,21 @@ public class HomeController
 		return "Index.jsp";
 	}
 	
-	@RequestMapping("add")
-	//HttpServletRequest request
-	public ModelAndView add(@RequestParam("NUM_1") int firstNum, @RequestParam("NUM_2") int secondNum, HttpSession session)
+	@RequestMapping(value="add", method=RequestMethod.GET)
+	public String add(HttpServletRequest request)
 	{
-		ModelAndView mv = new ModelAndView();
+		//int firstNum = Integer.parseInt(request.getParameter("NUM_1"));
+		//int secondNum = Integer.parseInt(request.getParameter("NUM_2"));
 		
-		mv.setViewName("result.jsp");
+		String s1 = request.getParameter("NUM_1");
+		String s2 = request.getParameter("NUM_2");
 		
+		String result = "Hello " + s1 + " " + s2;
 		
-		int result = firstNum + secondNum;
-		mv.addObject("result", result);
-		//session = request.getSession();
-		//session.setAttribute("result", result);
+		HttpSession session = request.getSession();
 		
-		return mv; //"result.jsp";
+		session.setAttribute("result", result);
+		
+		return "result.jsp";
 	}
 }
